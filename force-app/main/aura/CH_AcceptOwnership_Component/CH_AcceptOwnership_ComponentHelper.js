f58f225e8a390f8f69a176b134565376bec6b135
rajeswarr1 <rajeswarr1>
2023-12-08 21:03:17 +0000

    Committing selected files as part of Opsera Bulk retrieval task - 61e710fe9244e00018f85aed, run Count - 20.. Commit Number-0

---
+++ force-app/main/aura/CH_AcceptOwnership_Component/CH_AcceptOwnership_ComponentHelper.js
@@ -0,0 +1,13 @@
+({
+    
+    showToast: function(sType, title, message) {
+        var toastEvent = $A.get("e.force:showToast");
+        toastEvent.setParams({
+            "title": title,
+            "message": message,
+            "type": sType
+        });
+        toastEvent.fire();
+    },
+    
+})
